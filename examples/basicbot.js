1fcdf7818afae1b9a5382a09b1fe4b50fe08bbd9
Alexander Corn <mckay@doctormckay.com>
2015-07-21 13:04:18 -0400

    Added BasicBot example

---
+++ examples/basicbot.js
@@ -0,0 +1,37 @@
+/**
+ * SteamUser example - BasicBot
+ *
+ * Simply logs into Steam using account credentials and launches Team Fortress 2
+ * If you want to appear online, you'll need to use a Steam.SteamFriends handler
+ */
+
+var SteamUser = require('../index.js'); // Replace this with `require('steam-user');` if used outside of the module directory
+var client = new SteamUser();
+
+client.logOn({
+	"accountName": "username",
+	"password": "password"
+});
+
+client.on('loggedOn', function(details) {
+	console.log("Logged into Steam as " + client.steamID.getSteam3RenderedID());
+	client.gamesPlayed(440);
+});
+
+client.on('error', function(e) {
+	// Some error occurred during logon
+	console.log(e);
+});
+
+client.on('webSession', function(sessionID, cookies) {
+	console.log("Got web session");
+	// Do something with these cookies if you wish
+});
+
+client.on('newItems', function(count) {
+	console.log(count + " new items in our inventory");
+});
+
+client.on('emailInfo', function(address, validated) {
+	console.log("Our email address is " + address + " and it's " + (validated ? "validated" : "not validated"));
+});
