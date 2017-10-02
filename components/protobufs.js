9d4812dced64c91695d8ff374192598ea8d993af
Will Elwood <welwood08@users.noreply.github.com>
2017-10-02 03:35:57 +0100

    Fix #109
    Assumes directories won't have `.proto` suffix.

--- components/protobufs.js
+++ components/protobufs.js
@@ -3,7 +3,9 @@ var fs = require('fs');
 
 var builder = new Protobuf.Builder();
 fs.readdirSync(__dirname + '/../protobufs').forEach(function(file) {
-	Protobuf.loadProtoFile(__dirname + '/../protobufs/' + file, builder);
+	if (file.substr(-6) === '.proto') {
+		Protobuf.loadProtoFile(__dirname + '/../protobufs/' + file, builder);
+	}
 });
 
 module.exports = builder.build();
