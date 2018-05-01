c7cb684acdb873f6444684972f20281abf945432
Alex Corn <mckay@doctormckay.com>
2018-05-01 18:06:02 -0400

    Fixed tokendumper.js example script (closes #174)

--- examples/tokendumper.js
+++ examples/tokendumper.js
@@ -4,43 +4,36 @@
  * Dumps all access tokens available to anonymous user accounts.
  */
 
-var SteamUser = require('../index.js'); // Replace this with `require('steam-user');` if used outside of the module directory
+const SteamUser = require('../index.js'); // Replace this with `require('steam-user');` if used outside of the module directory
 
-var client = new SteamUser();
+let client = new SteamUser();
 client.logOn(); // Log onto Steam anonymously
 
-client.on('loggedOn', function(details) {
+client.on('loggedOn', (details) => {
 	console.log("Logged onto Steam as " + client.steamID.getSteam3RenderedID());
 
-	console.log("Requesting full list of appids and packageids from Steam");
-	client.getProductChanges(1, function(currentChangenumber, apps, packages) {
-		console.log("Got list of " + apps.length + " apps and " + packages.length + " packages, requesting tokens");
+	let idList = [];
+	for (let i = 0; i < 1000000; i++) {
+		idList.push(i);
+	}
 
-		apps = apps.map(function(app) {
-			return app.appid;
-		});
+	// doing this for packages just hangs forever
+	client.getProductAccessToken(idList, [], (appTokens, packageTokens, appDeniedTokens, packageDeniedTokens) => {
+		console.log("Tokens denied for " + appDeniedTokens.length + " apps and " + packageDeniedTokens.length + " packages");
 
-		packages = packages.map(function(pkg) {
-			return pkg.packageid;
-		});
-
-		client.getProductAccessToken(apps, packages, function(appTokens, packageTokens, appDeniedTokens, packageDeniedTokens) {
-			console.log("Tokens denied for " + appDeniedTokens.length + " apps and " + packageDeniedTokens.length + " packages");
-
-			for (var appid in appTokens) {
-				if (appTokens[appid].toString() != '0') {
-					console.log("App " + appid + ": " + appTokens[appid].toString());
-				}
+		for (let appid in appTokens) {
+			if (appTokens.hasOwnProperty(appid) && appTokens[appid].toString() != '0') {
+				console.log("App " + appid + ": " + appTokens[appid].toString());
 			}
+		}
 
-			for (var packageid in packageTokens) {
-				if (packageTokens[packageid].toString() != '0') {
-					console.log("Package " + packageid + ": " + packageTokens[packageid].toString());
-				}
+		for (let packageid in packageTokens) {
+			if (packageTokens.hasOwnProperty(packageid) && packageTokens[packageid].toString() != '0') {
+				console.log("Package " + packageid + ": " + packageTokens[packageid].toString());
 			}
+		}
 
-			console.log("Logging off of Steam");
-			client.logOff();
-		});
+		console.log("Logging off of Steam");
+		client.logOff();
 	});
 });
