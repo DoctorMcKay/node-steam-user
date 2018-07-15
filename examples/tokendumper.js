8dcfc733cc1f77e69732711e14d29e2eb8b52799
Royalgamer06 <royalgamer06@hotmail.com>
2018-07-15 21:46:11 +0200

    Using 10K appid chunks

--- examples/tokendumper.js
+++ examples/tokendumper.js
@@ -1,39 +1,51 @@
 /**
  * SteamUser example - TokenDumper
  *
- * Dumps all access tokens available to anonymous user accounts.
+ * Dumps access tokens for all apps available to anonymous user accounts.
  */
 
-const SteamUser = require('../index.js'); // Replace this with `require('steam-user');` if used outside of the module directory
+const SteamUser = require("steam-user");
+const fs = require("fs");
 
-let client = new SteamUser();
+const client = new SteamUser();
 client.logOn(); // Log onto Steam anonymously
 
-client.on('loggedOn', (details) => {
-	console.log("Logged onto Steam as " + client.steamID.getSteam3RenderedID());
+client.on("loggedOn", () => {
+    console.log("Logged onto Steam as " + client.steamID.getSteam3RenderedID());
 
-	let idList = [];
-	for (let i = 0; i < 1000000; i++) {
-		idList.push(i);
-	}
+    const chunksize = 10000; // seems best chunk size
+    const idList = [];
+    for (let i = chunksize; i < 1000000; i += chunksize) {
+        const idSubList = [];
+        for (let j = i - chunksize; j < i; j++) {
+            idSubList.push(j);
+        }
+        idList.push(idSubList);
+    }
 
-	// doing this for packages just hangs forever
-	client.getProductAccessToken(idList, [], (appTokens, packageTokens, appDeniedTokens, packageDeniedTokens) => {
-		console.log("Tokens denied for " + appDeniedTokens.length + " apps and " + packageDeniedTokens.length + " packages");
+    // doing this for all appids or packages just hangs forever
+    let tokenList = {};
+    getTokens();
 
-		for (let appid in appTokens) {
-			if (appTokens.hasOwnProperty(appid) && appTokens[appid].toString() != '0') {
-				console.log("App " + appid + ": " + appTokens[appid].toString());
-			}
-		}
+    function getTokens() {
+        const idSubList = idList.shift();
+        client.getProductAccessToken(idSubList, [], (appTokens, packageTokens, appDeniedTokens) => {
+            console.log("Tokens denied for " + appDeniedTokens.length + " apps of range " + idSubList[0] + "-" + idSubList[idSubList.length - 1]);
 
-		for (let packageid in packageTokens) {
-			if (packageTokens.hasOwnProperty(packageid) && packageTokens[packageid].toString() != '0') {
-				console.log("Package " + packageid + ": " + packageTokens[packageid].toString());
-			}
-		}
+            for (let appid in appTokens) {
+                if (appTokens.hasOwnProperty(appid) && appTokens[appid].toString() !== "0") {
+                    console.log("App " + appid + ": " + appTokens[appid].toString());
+                    tokenList[appid] = appTokens[appid].toString();
+                }
+            }
 
-		console.log("Logging off of Steam");
-		client.logOff();
-	});
+            if (idList.length > 0) {
+                getTokens();
+            } else {
+                fs.writeFileSync("tokens.json", JSON.stringify(tokenList, null, 4), "utf8");
+                console.log("Logging off of Steam");
+                client.logOff();
+            }
+        });
+    }
 });
