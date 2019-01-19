a632a06c7112beee61b5d95970fed36f245c7a94
Alex Corn <mckay@doctormckay.com>
2019-01-19 01:10:11 -0500

    Bring back old protobuf.js behavior of filling blank fields with null
    (fixes #207)

--- components/messages.js
+++ components/messages.js
@@ -288,7 +288,51 @@ exports.decodeProto = function(proto, encoded) {
 	}
 
 	let decoded = proto.decode(encoded);
-	return proto.toObject(proto.decode(encoded), {"defaults": true, "longs": String});
+	let objNoDefaults = proto.toObject(decoded, {"longs": String});
+	let objWithDefaults = proto.toObject(decoded, {"defaults": true, "longs": String});
+	return replaceDefaults(objNoDefaults, objWithDefaults);
+
+	function replaceDefaults(noDefaults, withDefaults) {
+		if (Array.isArray(withDefaults)) {
+			return withDefaults.map((val, idx) => replaceDefaults(noDefaults[idx], val));
+		}
+
+		for (let i in withDefaults) {
+			if (!withDefaults.hasOwnProperty(i)) {
+				continue;
+			}
+
+			if (withDefaults[i] && typeof withDefaults[i] === 'object' && !Buffer.isBuffer(withDefaults[i])) {
+				// Covers both object and array cases, both of which will work
+				// Won't replace empty arrays, but that's desired behavior
+				withDefaults[i] = replaceDefaults(noDefaults[i], withDefaults[i]);
+			} else if (typeof noDefaults[i] === 'undefined' && isReplaceableDefaultValue(withDefaults[i])) {
+				withDefaults[i] = null;
+			}
+		}
+
+		return withDefaults;
+	}
+
+	function isReplaceableDefaultValue(val) {
+		if (Buffer.isBuffer(val) && val.length == 0) {
+			// empty buffer is replaceable
+			return true;
+		}
+
+		if (Array.isArray(val)) {
+			// empty array is not replaceable (empty repeated fields)
+			return false;
+		}
+
+		if (val === '0') {
+			// Zero as a string is replaceable (64-bit integer)
+			return true;
+		}
+
+		// Anything falsy is true
+		return !val;
+	}
 };
 
 /**
