23f49a2800f9113899122e0848eee62cec5cb981
Revadike <revadike@outlook.com>
2019-04-25 19:42:59 +0200

    const -> let
    I know... I don't like it either

--- examples/tokendumper.js
+++ examples/tokendumper.js
@@ -7,31 +7,31 @@
 const SteamUser = require('../index.js'); // Replace this with `require('steam-user');` if used outside of the module directory
 const FS = require('fs');
 
-const client = new SteamUser();
+let client = new SteamUser();
 client.logOn(); // Log onto Steam anonymously
 
 client.on('loggedOn', () => {
 	console.log("Logged onto Steam as " + client.steamID.steam3());
 
-	const chunksize = 10000; // seems best chunk size
-	const idList = [];
-	for (const i = chunksize; i < 1000000; i += chunksize) {
-		const idSubList = [];
-		for (const j = i - chunksize; j < i; j++) {
+	let chunksize = 10000; // seems best chunk size
+	let idList = [];
+	for (let i = chunksize; i < 1000000; i += chunksize) {
+		let idSubList = [];
+		for (let j = i - chunksize; j < i; j++) {
 			idSubList.push(j);
 		}
 		idList.push(idSubList);
 	}
 
 	// doing this for all appids or packages just hangs forever
-	const tokenList = {};
+	let tokenList = {};
 
-	const getTokens = () => {
-		const idSubList = idList.shift();
+	let getTokens = () => {
+		let idSubList = idList.shift();
 		client.getProductAccessToken(idSubList, [], (appTokens, packageTokens, appDeniedTokens) => {
 			console.log("Tokens denied for " + appDeniedTokens.length + " apps of range " + idSubList[0] + "-" + idSubList[idSubList.length - 1]);
 
-			for (const appid in appTokens) {
+			for (let appid in appTokens) {
 				if (appTokens.hasOwnProperty(appid) && appTokens[appid].toString() !== "0") {
 					console.log("App " + appid + ": " + appTokens[appid].toString());
 					tokenList[appid] = appTokens[appid].toString();
