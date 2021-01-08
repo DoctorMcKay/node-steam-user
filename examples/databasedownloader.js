4a7a072dbdd00ed2b2e833f6d237077d1d869991
Alex Corn <alex@alexcorn.com>
2021-01-08 05:27:23 -0500

    Automatically request access tokens in databasedownloader example

--- examples/databasedownloader.js
+++ examples/databasedownloader.js
@@ -14,7 +14,7 @@ client.on('loggedOn', async (details) => {
 	console.log("Logged onto Steam as " + client.steamID.steam3());
 
 	console.log("Requesting appinfo for TF2 and CS:GO...");
-	let result = await client.getProductInfo([440, 730], []);
+	let result = await client.getProductInfo([440, 730], [], true); // Passing true as the third argument automatically requests access tokens, which are required for some apps
 
 	console.log("Got app info, writing to files");
 	console.log(result);
