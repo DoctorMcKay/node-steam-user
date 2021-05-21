bb50d2ed30e2588f61d48616b81225c444768815
Revadike <revadike@outlook.com>
2021-05-21 22:39:30 +0200

    Include free promotions in owned apps

--- components/apps.js
+++ components/apps.js
@@ -572,8 +572,8 @@ SteamUser.prototype.getOwnedApps = function(excludeSharedLicenses) {
 
 		pkg = pkg.packageinfo;
 
-		if (pkg.extended && pkg.extended.expirytime && pkg.extended.expirytime <= Math.floor(Date.now() / 1000)) {
-			return; // This package has expired. Free weekend, usually
+		if (pkg.extended && pkg.extended.expirytime && pkg.extended.expirytime <= Math.floor(Date.now() / 1000) && !pkg.extended.freepromotion) {
+			return; // This package has expired. Free weekend, usually. Free promotions are yours to keep permanently, though.
 		}
 
 		(pkg.appids || []).forEach((appid) => {
