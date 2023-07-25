49506fda2f6de87e10f80f765c17d2311660f114
Alex Corn <alex@alexcorn.com>
2023-07-25 04:11:23 -0400

    Removed setSentry() and sentry event

--- components/08-machineauth.js
+++ components/08-machineauth.js
@@ -6,15 +6,6 @@ const SteamUserBase = require('./00-base.js');
 const SteamUserWeb = require('./07-web.js');
 
 class SteamUserMachineAuth extends SteamUserWeb {
-	/**
-	 * @param {Buffer|string} sentry
-	 * @deprecated Use `machineAuthToken` property in `logOn()` instead
-	 */
-	setSentry(sentry) {
-		this._machineAuthTokenSetByDeprecatedSetSentry = true;
-		this._machineAuthToken = Buffer.isBuffer(sentry) ? sentry.toString('utf8') : sentry;
-	}
-
 	_getMachineAuthFilename() {
 		let accountName = (this._logOnDetails.account_name || this._logOnDetails._newAuthAccountName).toLowerCase();
 		return `machineAuthToken.${accountName}.txt`;
@@ -24,7 +15,6 @@ class SteamUserMachineAuth extends SteamUserWeb {
 		this._machineAuthToken = machineToken;
 
 		this.emit('machineAuthToken', machineToken);
-		this.emit('sentry', Buffer.from(machineToken, 'utf8')); // legacy
 
 		// We should always have an account name available here since this is only possible when logging on using an
 		// account name and password.
